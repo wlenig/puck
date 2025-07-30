47e2c343b7536d6bebbca916c673ecbdecd944f6
Chris Villa <chrisvilla@me.com>
2025-07-30 19:33:21 +0100

    chore: add value to useWhyRender callback

--- packages/core/lib/use-why-render.ts
+++ packages/core/lib/use-why-render.ts
@@ -2,12 +2,12 @@ import { useEffect } from "react";
 
 export const useWhyRender = (
   obj: Record<string, any>,
-  onRender: (key: string) => void
+  onRender: (key: string, val: any) => void
 ) => {
   Object.keys(obj).map((key) => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
-      onRender(key);
+      onRender(key, obj[key]);
     }, [obj[key]]);
   });
 };
